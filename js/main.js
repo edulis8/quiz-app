efe95beb80561fa612cadb742b266bb2eefbebb9
edulis8 <eric_broberg@hotmail.com>
2015-09-22 14:27:29 -0700

    added jquery quiz functionality

---
+++ js/main.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+	
+	var questions = [
+		{
+			question: "What is the botanical name of the goldenberry plant?",
+			answers: ["Physalis peruviana", "Physalis alkekengi", "Physalis philadelphica",
+			 "Physalis pubescens"],
+			placeNumber: 0,
+			correct: 0
+		},
+		{
+			question: "Where is the goldenberry from?",
+			answers: ["South Africa", "Hawaii", "South America", "India"],
+			placeNumber: 1,
+			correct: 2
+		},
+		{
+			question: "Where are most goldenberries imported inot the U.S. from?",
+			answers: ["Peru", "Colombia", "New Zealand", "Turkey"],
+			placeNumber: 2,
+			correct: 1
+		},
+		{
+			question: "What are goldenberries most closely related to?",
+			answers: ["Tomatillos", "Gooseberries", "Tomatoes", "Blueberries"],
+			placeNumber: 3,
+			correct: 0
+		},
+	]
+
+// global variables
+
+var numberCorrect= 0;
+var currentQuestion= 0;
+
+//event listeners
+
+$('button').click(function(event) {
+	if(currentQuestion < 4){
+		checkCorrectness();
+		postNewTotal();
+		currentQuestion++;
+		nextQuestion();
+	}
+	console.log(currentQuestion)
+});
+
+
+
+//functions
+
+function checkCorrectness(){
+
+	var answer = $("input[type='radio']:checked").val();
+	var correctAnswer = questions[currentQuestion].correct;
+
+	
+
+	if(answer == correctAnswer){
+		numberCorrect++;
+		//console.log(numberCorrect);
+	}
+}
+
+function postNewTotal(){
+	$('span#num-correct').html(numberCorrect);
+
+}
+
+function nextQuestion(){
+
+	if(currentQuestion < 4){
+	
+		$('div#answerholder'+currentQuestion).hide(); //if current question is 1, hides #answerholder1
+		$('div#answerholder'+(currentQuestion+1)).show(); //shows #answerholder2
+	}
+
+	else{
+		$('div#answerholder'+currentQuestion).hide(); 
+		$('span#finalnumber').html(numberCorrect);
+		$('#final-div').show();
+	}
+}
+
+
+
+});
